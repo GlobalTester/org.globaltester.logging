4d61915fc51a1b23143e07e5939c911a3a66e26d
Julian Koch <julian.koch@hjp-consulting.com>
2015-04-29 12:03:26 +0200

    Introduced a service for filtering log messages

---
+++ org.globaltester.logging.filterService/src/org/globaltester/logging/filterservice/LogFilterService.java
@@ -0,0 +1,40 @@
+package org.globaltester.logging.filterservice;
+
+import java.util.LinkedList;
+
+import org.osgi.service.log.LogEntry;
+
+/**
+ * Service for filtering log messages
+ * 
+ * @author jkoch
+ *
+ */
+public interface LogFilterService {	
+	
+	public void setLogLevels(byte[] loglevels);
+	
+	public byte [] getLogLevels();
+	
+	public void setBundleFilters(LinkedList<String> bundleFilters);
+	
+	/**
+	 * saves filter data in the preferences
+	 */
+	public void saveFilter();
+	
+	/**
+	 * loads filter data from preferences
+	 */
+	public void loadFilter();
+	
+	/**
+	 * Filters a LogEntry object and return true or false if the LogEntry object
+	 * doesn't fit the requirements
+	 * 
+	 * @param entry
+	 *            the LogEntry object which contains the message to log
+	 */
+	public boolean logFilter(LogEntry entry);
+
+}
