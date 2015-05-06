e30bd7943fd9320b7343d497878f0d2a7fdcdef8
Julian Koch <julian.koch@hjp-consulting.com>
2015-05-06 11:54:45 +0200

    Replaced the checkFilter() method with a getFilter() method. Also provided a getFormat() method.

--- org.globaltester.logging.filterService/src/org/globaltester/logging/filterservice/LogReaderConfig.java
+++ org.globaltester.logging.filterService/src/org/globaltester/logging/filterservice/LogReaderConfig.java
@@ -1,22 +1,25 @@
 package org.globaltester.logging.filterservice;
 
 import org.globaltester.logging.formatservice.LogFormat;
-import org.osgi.service.log.LogEntry;
 
 public class LogReaderConfig {
 	
 	byte logLevels [] ={1,2,3,4,5,6,120};
 	String bundleList [] = {"de.persosim.simulator"};
 	
-	public LogFormat formatter = new LogFormat();
+	public LogFormat format = new LogFormat();
 	public BundleFilter bundleFilter = new BundleFilter(bundleList);
 	public LevelFilter levelFilter = new LevelFilter(logLevels);
 	
 	
-	LogFilterService [] filters = {bundleFilter, levelFilter};
-	public AndFilter andFilter = new AndFilter(filters);
+	LogFilter [] filters = {bundleFilter, levelFilter};
+	public AndFilter filter = new AndFilter(filters);
 	
-	public boolean checkFilter(LogEntry entry){		
-		return andFilter.perform(entry);
+	public LogFilter getFilter() {		
+		return filter;
+	}
+	
+	public LogFormat getFormat() {		
+		return format;
 	}
 }
