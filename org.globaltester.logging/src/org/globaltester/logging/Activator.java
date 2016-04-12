76c9c7b36cb359a27aca214a87e43bbcd0180ed3
Christian Stroh <christian.stroh@hjp-consulting.com>
2016-04-12 11:59:27 +0200

    PersoSimLogger moved from d.p.s to o.g.logging

---
+++ org.globaltester.logging/src/org/globaltester/logging/Activator.java
@@ -0,0 +1,40 @@
+package org.globaltester.logging;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.log.LogService;
+import org.osgi.util.tracker.ServiceTracker;
+
+public class Activator implements BundleActivator {
+
+	public static BundleContext context;
+	
+	//TODO move this service tracking to the class PersoSimLogger (similar to Crypto)
+	private static ServiceTracker<LogService, LogService> logServiceTracker;
+	
+	public static LogService getLogservice() {
+		if (logServiceTracker != null){
+			return logServiceTracker.getService();
+		}
+		return null;
+	}
+	
+	@Override
+	public void start(BundleContext context) throws Exception {
+		Activator.context = context;
+		//get LogService
+		logServiceTracker = new ServiceTracker<LogService, LogService>(context, LogService.class.getName(), null);
+		logServiceTracker.open();
+}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		Activator.context = null;
+		logServiceTracker.close();
+	}
+	
+	public static BundleContext getContext() {
+		return context;
+	}
+
+}
