12dad92675600bf7749d284ab8db6de4526c56a8
Martin Boonk <martin.boonk@hjp-consulting.com>
2016-05-18 11:26:03 +0200

    Allow additionalData in LogTag objects

--- org.globaltester.logging/src/org/globaltester/logging/tags/LogTag.java
+++ org.globaltester.logging/src/org/globaltester/logging/tags/LogTag.java
@@ -1,16 +1,23 @@
 package org.globaltester.logging.tags;
 
+import java.util.Arrays;
+
 public class LogTag {
 
 	protected String id;
+	private String [] additionalData;
 
-	public LogTag(String id) {
-		super();
+	public LogTag(String id, String ... additionalData) {
 		this.id = id;
+		this.additionalData = additionalData;
 	}
 
 	public String getId() {
 		return id;
+	}
+
+	public String [] getAdditionalData (){
+		return Arrays.copyOf(additionalData, additionalData.length);
 	}
 
 }
