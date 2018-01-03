19525df1e34e37179b4430f0dcf32c37f3526732
Martin Boonk <martin.boonk@secunet.com>
2018-01-03 14:07:13 +0100

    Format json messages in console logger

--- org.globaltester.logging.consoleLogger/src/org/globaltester/logging/consolelogger/ConsoleLogger.java
+++ org.globaltester.logging.consoleLogger/src/org/globaltester/logging/consolelogger/ConsoleLogger.java
@@ -1,6 +1,7 @@
 package org.globaltester.logging.consolelogger;
 
 import org.globaltester.logging.AbstractLogListener;
+import org.json.JSONObject;
 import org.osgi.service.log.LogEntry;
 import org.osgi.service.log.LogListener;
 
@@ -14,10 +15,11 @@ public class ConsoleLogger extends AbstractLogListener {
 
 	@Override
 	public void displayLogMessage(String msg) {
-		// cut at line breaks and print
-		String[] splitResult = msg.split("(\\n|\\r)");
-		for (int i = 0; i < splitResult.length; i++) {
-			System.out.println(splitResult[i]);	
+		try {
+			System.out.println(msg.substring(0, msg.indexOf(']') + 1) + " " + new JSONObject(msg.substring(msg.indexOf(']') + 1)).getJSONObject("org.globaltester.logging.Message").get("messageContent"));
+		} catch (Exception e) {
+			//NOSONAR: most resilient way for logging is simply printing and we have no control over actual formatting of incoming messages
+			System.out.println(msg);
 		}
 	}
 }
